335f6bf76252ac050e4c02d6125bd1331f237593
Clay Webb <clay_webb@outlook.com>
2021-12-11 18:12:42 -0600

    Completed Assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -72,7 +72,6 @@ else {
 }
 
 
-
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -82,7 +81,9 @@ else {
 // console.log `mostPets` after the conditional has run.
 
 
+
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
+
 
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
